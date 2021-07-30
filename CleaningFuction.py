00321b3d045b068a94cd55587c9c6f5499231f78
dereksgithub <derekding727@gmail.com>
2021-07-30 16:48:20 +0800

    Developing the rest of the proj following the block diagram new design.

--- CleaningFuction.py
+++ CleaningFuction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import echarts as ech
+import os
 
 
 
@@ -14,4 +15,23 @@ import echarts as ech
 # Dir where all the datasets are stored
 data_dir = r"D:\Downloads\Stackoverflow_Data _2011_to_2020\all"
 
+# iterate through the folder to read the data into corresponding dataframes,
+# files is the list to store all the file names:
+files = []
 
+for r, d, f in os.walk(data_dir): # r=root, d=directories, f = files
+    for file in f:
+        if '.csv' in file:
+            files.append(os.path.join(r, file))
+print(files)
+
+# read CSV function reads the CSV file and perform initial data wrangling on the file.
+
+
+
+
+
+
+
+
+#
