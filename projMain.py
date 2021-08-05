336094b55bf570fb71631a1f254679056daf4ad7
dereksgithub <derekding727@gmail.com>
2021-08-05 10:56:43 +0800

    added comments and desc

--- projMain.py
+++ projMain.py
@@ -1,6 +1,19 @@
-
-# this is the module used for visualizing dataframes
-
+# import all libraries
 
 
-df1 =CleaningFunction()
+# calling all the cleaning functions and clean the datasets into 10 dataframes that we can perform
+# meaningful analysis
+df11 = CleaningFunction()
+df12 = CleaningFunction()
+df13 = CleaningFunction()
+df14 = CleaningFunction()
+df15 = CleaningFunction()
+df16 = CleaningFunction()
+df17 = CleaningFunction()
+df18 = CleaningFunction()
+df19 = CleaningFunction()
+df20 = CleaningFunction()
+
+# basic statistics and visualizations:
+
+# using our analysis answer the business questions
