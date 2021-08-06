c9d17b76cb2d5c0f4a3b64018b07ae747afde310
dereksgithub <derekding727@gmail.com>
2021-08-06 19:31:47 +0800

    updated cleaning function for 2011 and 2012

--- cleaningDataframes.py
+++ cleaningDataframes.py
@@ -1,20 +1,43 @@
 #
+import numpy as np
 import pandas as pd
 
-def clean2011data(questionOption2011):
-    # test place holder
+def clean2011data(df11, questionoption2011):
+    # display the reminder that the program has started
     print("extracting data from the 2011 survey data")
+
+    # place holder
+    print(df11)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
-    if questionOption2011 == 1:
+    if questionoption2011 == 1:
+        # process the 2011 dataset and get the information needed only for question and return the dataframe
         print("question1")
-    elif questionOption2011 == 2:
+    elif questionoption2011 == 2:
+        #
         print("question2")
     else:
-        print("question3")
-def clean2012data(questionOption2012):
-    # test place holder
+        # here we take the columns that contains languages merge columns
+        df11['Prog_languages'] = df11[df11.columns[31:43]].apply(
+            lambda x: ';'.join(x.dropna().astype(str)),
+            axis=1)
+        # rename the merged column
+        dfout = pd.DataFrame(df11['Prog_languages'])
+        # add the 2011 year column
+        dfout['Year'] = '2011'
+        # testing
+        # print(dfout)
+        return(dfout)
+
+
+
+def clean2012data(df12, questionOption2012):
+    # display the reminder that the program has started
     print("extracting data from the 2012 survey data")
+    # place holder
+    print(df12)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2012 == 1:
@@ -22,12 +45,25 @@ def clean2012data(questionOption2012):
     elif questionOption2012 == 2:
         print("question2")
     else:
-        print("question3")
+        # here we take the columns that contains languages merge columns
+        df12['Prog_languages'] = df12[df12.columns[23:37]].apply(
+            lambda x: ';'.join(x.dropna().astype(str)),
+            axis=1)
+        # rename the merged column
+        dfout = pd.DataFrame(df12['Prog_languages'])
+        # add the 2012 year column
+        dfout['Year'] = '2012'
+        # testing
+        print(dfout)
+        return (dfout)
 
 
-def clean2013data(questionOption2013):
-    # test place holder
+def clean2013data(df13, questionOption2013):
+    # display the reminder that the program has started
     print("extracting data from the 2013 survey data")
+    # place holder
+    print(df13)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2013 == 1:
@@ -37,9 +73,13 @@ def clean2013data(questionOption2013):
     else:
         print("question3")
 
-def clean2014data(questionOption2014):
-    # test place holder
+
+def clean2014data(df14, questionOption2014):
+    # display the reminder that the program has started
     print("extracting data from the 2014 survey data")
+    # place holder
+    print(df14)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2014 == 1:
@@ -49,9 +89,13 @@ def clean2014data(questionOption2014):
     else:
         print("question3")
 
-def clean2015data(questionOption2015):
-    # test place holder
+
+def clean2015data(df15, questionOption2015):
+    # display the reminder that the program has started
     print("extracting data from the 2015 survey data")
+    # place holder
+    print(df15)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2015 == 1:
@@ -61,9 +105,13 @@ def clean2015data(questionOption2015):
     else:
         print("question3")
 
-def clean2016data(questionOption2016):
-    # test place holder
+
+def clean2016data(df16, questionOption2016):
+    # display the reminder that the program has started
     print("extracting data from the 2016 survey data")
+    # place holder
+    print(df16)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2016 == 1:
@@ -74,9 +122,12 @@ def clean2016data(questionOption2016):
         print("question3")
 
 
-def clean2017data(questionOption2017):
+def clean2017data(df17, questionOption2017):
     # display the reminder that the program has started
     print("extracting data from the 2017 survey data")
+    # place holder
+    print(df17)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2017 == 1:
@@ -87,9 +138,12 @@ def clean2017data(questionOption2017):
         print("question3")
 
 
-def clean2018data(questionOption2018):
+def clean2018data(df18, questionOption2018):
     # display the reminder that the program has started
     print("extracting data from the 2018 survey data")
+    # place holder
+    print(df18)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2018 == 1:
@@ -99,9 +153,13 @@ def clean2018data(questionOption2018):
     else:
         print("question3")
 
-def clean2019data(questionOption2019):
+
+def clean2019data(df19, questionOption2019):
     # display the reminder that the program has started
     print("extracting data from the 2019 survey data")
+    # place holder
+    print(df19)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2019 == 1:
@@ -111,9 +169,13 @@ def clean2019data(questionOption2019):
     else:
         print("question3")
 
-def clean2020data(questionOption2020):
+
+def clean2020data(df20, questionOption2020):
     # display the reminder that the program has started
     print("extracting data from the 2020 survey data")
+    # place holder
+    print(df20)
+
     # option = 1 means we use the cleaning function to clean the dataframe and get information needed to answer business
     # question 1
     if questionOption2020 == 1:
--- projMain.py
+++ projMain.py
@@ -2,45 +2,59 @@
 import cleaningDataframes
 import statFunction
 import visualizationModule
+import pandas as pd
 
+# read the data frames
+df11 = pd.read_csv("2011 Stack Overflow Survey Responses.csv", encoding='latin-1')
+df12 = pd.read_csv("2012 Stack Overflow Survey Responses.csv", encoding='latin-1')
+df13 = 2013
+df14 = 2014
+df15 = 2015
+df16 = 2016
+df17 = 2017
+df18 = 2018
+df19 = 2019
+df20 = 2020
 # calling all the cleaning functions and clean the datasets into 10 dataframes that we can perform
 # meaningful analysis
 # options 1 get the data from each year for business question 1
-df11q1 = cleaningDataframes.clean2011data(1)
-df12q1 = cleaningDataframes.clean2012data(1)
-df13q1 = cleaningDataframes.clean2013data(1)
-df14q1 = cleaningDataframes.clean2014data(1)
-df15q1 = cleaningDataframes.clean2015data(1)
-df16q1 = cleaningDataframes.clean2016data(1)
-df17q1 = cleaningDataframes.clean2017data(1)
-df18q1 = cleaningDataframes.clean2018data(1)
-df19q1 = cleaningDataframes.clean2019data(1)
-df20q1 = cleaningDataframes.clean2020data(1)
+df11q1 = cleaningDataframes.clean2011data(df11, 1)
+df12q1 = cleaningDataframes.clean2012data(df12, 1)
+df13q1 = cleaningDataframes.clean2013data(df13, 1)
+df14q1 = cleaningDataframes.clean2014data(df14, 1)
+df15q1 = cleaningDataframes.clean2015data(df15, 1)
+df16q1 = cleaningDataframes.clean2016data(df16, 1)
+df17q1 = cleaningDataframes.clean2017data(df17, 1)
+df18q1 = cleaningDataframes.clean2018data(df18, 1)
+df19q1 = cleaningDataframes.clean2019data(df19, 1)
+df20q1 = cleaningDataframes.clean2020data(df20, 1)
 
 # options 2 get the data from each year for business question 2
-df11q2 = cleaningDataframes.clean2011data(2)
-df12q2 = cleaningDataframes.clean2012data(2)
-df13q2 = cleaningDataframes.clean2013data(2)
-df14q2 = cleaningDataframes.clean2014data(2)
-df15q2 = cleaningDataframes.clean2015data(2)
-df16q2 = cleaningDataframes.clean2016data(2)
-df17q2 = cleaningDataframes.clean2017data(2)
-df18q2 = cleaningDataframes.clean2018data(2)
-df19q2 = cleaningDataframes.clean2019data(2)
-df20q2 = cleaningDataframes.clean2020data(2)
+df11q2 = cleaningDataframes.clean2011data(df11, 2)
+df12q2 = cleaningDataframes.clean2012data(df12, 2)
+df13q2 = cleaningDataframes.clean2013data(df13, 2)
+df14q2 = cleaningDataframes.clean2014data(df14, 2)
+df15q2 = cleaningDataframes.clean2015data(df15, 2)
+df16q2 = cleaningDataframes.clean2016data(df16, 2)
+df17q2 = cleaningDataframes.clean2017data(df17, 2)
+df18q2 = cleaningDataframes.clean2018data(df18, 2)
+df19q2 = cleaningDataframes.clean2019data(df19, 2)
+df20q2 = cleaningDataframes.clean2020data(df20, 2)
 
 
 # options 3 get the data from each year for business question 3
-df11q3 = cleaningDataframes.clean2011data(3)
-df12q3 = cleaningDataframes.clean2012data(3)
-df13q3 = cleaningDataframes.clean2013data(3)
-df14q3 = cleaningDataframes.clean2014data(3)
-df15q3 = cleaningDataframes.clean2015data(3)
-df16q3 = cleaningDataframes.clean2016data(3)
-df17q3 = cleaningDataframes.clean2017data(3)
-df18q3 = cleaningDataframes.clean2018data(3)
-df19q3 = cleaningDataframes.clean2019data(3)
-df20q3 = cleaningDataframes.clean2020data(3)
+# 2011 Cleaning function ready
+# df11q3 = cleaningDataframes.clean2011data(df11, 3)
+# 2012 Cleaning function ready
+# df12q3 = cleaningDataframes.clean2012data(df12, 3)
+df13q3 = cleaningDataframes.clean2013data(df13, 3)
+df14q3 = cleaningDataframes.clean2014data(df14, 3)
+df15q3 = cleaningDataframes.clean2015data(df15, 3)
+df16q3 = cleaningDataframes.clean2016data(df16, 3)
+df17q3 = cleaningDataframes.clean2017data(df17, 3)
+df18q3 = cleaningDataframes.clean2018data(df18, 3)
+df19q3 = cleaningDataframes.clean2019data(df19, 3)
+df20q3 = cleaningDataframes.clean2020data(df20, 3)
 
 
 # merge 11 to 20 dataframes for analysis over 10 years
