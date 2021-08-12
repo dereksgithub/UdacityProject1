441a102cef08ff6b83c58bb579a473eb14d1d70a
dereksgithub <derekding727@gmail.com>
2021-08-12 11:01:20 +0800

    completed data formatting function moving on to visualization

--- visualizationModule.py
+++ visualizationModule.py
@@ -1,25 +1,40 @@
 # from pyecharts.charts import Bar
 # from pyecharts import options as opts
+from operator import itemgetter
+from collections import Counter
 
 def list_basic_viz():
     print("visualization")
 
 
 # format the data for question and visualization
-def q3_data_format(dfin):
+def q3_data_format(df_in, year_df):
     # print the reminder message
     print("Formatting the cleaned dataframes for visualization")
+    # get the total number of rows of cleaned data
+    total_num_rows = len(df_in.index)
     # get the column section and use as a list
-    all_list = dfin["Prog_languages"].tolist()
+    all_list = df_in["Prog_languages"].tolist()
     # create a list to store all the languages mentioned in the survey response
     cleaned_list = []
     for i in all_list:
-        m_list = i.split(";").strip()
+        m_list = i.split(";")
         for m in m_list:
-            cleaned_list.append(m)
+            cleaned_list.append(m.strip())
     # use the Counter() function from collections
     # to count the number of appearances for each language
-
+    lang_year = Counter(cleaned_list)
+    # language of this year in dict format:
+    lang_year = dict(lang_year)
+    # transform each language's appearance in "%"
+    for key in lang_year:
+        lang_year[key] = (lang_year[key] / total_num_rows)*100
+    # keep only the top 20 results
+    res_year = dict(sorted(lang_year.items(), key=itemgetter(1), reverse=True)[:20])
+    # add the year information
+    res_year['Year'] = year_df
+    # return the dict for plotting
+    return res_year
 
 def q3_visualization():
     # take the
