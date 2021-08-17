34e940d8d84fb678bba5acfc53351ee2b6585f50
dereksgithub <derekding727@gmail.com>
2021-08-17 10:29:21 +0800

    completed the over the 10 years plot, too messy however, maybe keep only 10 top results

--- visualizationModule.py
+++ visualizationModule.py
@@ -6,8 +6,9 @@ from pyecharts.charts import Bar
 # from pyecharts import options as opts
 import numpy as np
 import pyecharts.options as opts
-from pyecharts.charts import Line
+from pyecharts.charts.basic_charts.line import Line
 import pandas as pd
+
 
 # basic visualization for any cleaned dataframe:
 def list_basic_viz():
@@ -121,50 +122,42 @@ def q3_visualization_for_all_years(*argv):
     df_plotting = df_cleaned.round(2)
 
     # print(df_plotting["Year"])
-    for col in df_cleaned.columns:
-        print(col)
+    # for col in df_cleaned.columns:
+    #    print(col)
 
     # start the plotting section:
     week_name_list = df_plotting["Year"]
-    high_temperature = [11, 11, 15, 13, 12, 13, 10, 5, 5, 10]
-    low_temperature = [1, -2, 2, 5, 3, 2, 0, 0, 1, 2]
+    # initialize the line object
+    line = Line(init_opts=opts.InitOpts(width="2000px", height="1600px"))
+    line.add_xaxis(xaxis_data=week_name_list)
+    for col in df_cleaned.columns:
+        if col != "Year":
+            line.add_yaxis(series_name=col, y_axis=df_cleaned[col])
+        # markpoint_opts=opts.MarkPointOpts(
+        # data=[ opts.MarkPointItem(type_="max", name="最大值"),
+        #         opts.MarkPointItem(type_="min", name="最小值"), ] ),
+        # markline_opts=opts.MarkLineOpts(
+        #      data=[opts.MarkLineItem(type_="average", name="平均值")] ),)
+    # line.add_yaxis(
+    #         series_name="最低气温",
+    #         y_axis=low_temperature,
+    #         markpoint_opts=opts.MarkPointOpts(
+    #             data=[opts.MarkPointItem(value=-2, name="low point", x=1, y=-1.5)]
+    #         ),
+    #         markline_opts=opts.MarkLineOpts(
+    #             data=[
+    #                 # opts.MarkLineItem(type_="average", name="Average"),
+    #                 # opts.MarkLineItem(symbol="none", x="90%", y="max"),
+    #                 opts.MarkLineItem(symbol="circle", type_="max", name="high point"),
+    #             ]
+    #         ),
+    # )
 
-    (
-        Line(init_opts=opts.InitOpts(width="1600px", height="800px"))
-            .add_xaxis(xaxis_data=week_name_list)
-            .add_yaxis(
-            series_name="最高气温",
-            y_axis=high_temperature,
-            markpoint_opts=opts.MarkPointOpts(
-                data=[
-                    opts.MarkPointItem(type_="max", name="最大值"),
-                    opts.MarkPointItem(type_="min", name="最小值"),
-                ]
-            ),
-            markline_opts=opts.MarkLineOpts(
-                data=[opts.MarkLineItem(type_="average", name="平均值")]
-            ),
-        )
-            .add_yaxis(
-            series_name="最低气温",
-            y_axis=low_temperature,
-            markpoint_opts=opts.MarkPointOpts(
-                data=[opts.MarkPointItem(value=-2, name="周最低", x=1, y=-1.5)]
-            ),
-            markline_opts=opts.MarkLineOpts(
-                data=[
-                    # opts.MarkLineItem(type_="average", name="Average"),
-                    # opts.MarkLineItem(symbol="none", x="90%", y="max"),
-                    opts.MarkLineItem(symbol="circle", type_="max", name="最高点"),
-                ]
-            ),
-        )
-            .set_global_opts(
-            title_opts=opts.TitleOpts(title="% of Programming Language from the Survey ",
-                                      subtitle="% of people from the survey who uses this Language"),
-            tooltip_opts=opts.TooltipOpts(trigger="axis"),
-            toolbox_opts=opts.ToolboxOpts(is_show=False),
-            xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
-        )
-            .render("example_pyechart_line_chart_2.html")
+    line.set_global_opts(
+        title_opts=opts.TitleOpts(title="% of Programming Language from the Survey ",
+                                  subtitle="% of people from the survey who uses this Language"),
+        tooltip_opts=opts.TooltipOpts(trigger="axis"),
+        toolbox_opts=opts.ToolboxOpts(is_show=False),
+        xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
     )
+    line.render("example_pyechart_line_chart_2.html")
